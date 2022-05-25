554cd753288a897c981e320d9ccf5db82c66b5da
Tugsem <saranlitugsem@gmail.com>
2022-05-25 15:29:36 +0300

    Change App function component to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Calculator from './components/Calculator';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
     <div className="App">
       <Calculator />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
